b3a26ab957c22f6cb878fabc9700714331b62e1b
Guillermo De La Cruz Guzmán <gdlcruzguzman@gmail.com>
2021-10-18 10:55:51 +0200

    Ejercicios tema-2 EC02

---
+++ UD2-EC02-EJ1-DelacruzGuillermo/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE country (
+    id   INTEGER      NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128) NOT NULL,
+    PRIMARY KEY (id)
+);
+insert into country values (2, 'Francia')
+--Spring boot ejecuta schema o data directamente
