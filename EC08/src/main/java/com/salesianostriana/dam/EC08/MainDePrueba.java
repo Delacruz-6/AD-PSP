d75b5f0a8b937abe70ac62b29488ec8568432ba3
Delacruz-6 <gdlcruzguzman@gmail.com>
2021-10-27 13:26:20 +0200

    add

---
+++ EC08/src/main/java/com/salesianostriana/dam/EC08/MainDePrueba.java
@@ -0,0 +1,4 @@
+package com.salesianostriana.dam.EC08;
+
+public class MainDePrueba {
+}
---
+++ EC08/src/main/java/com/salesianostriana/dam/EC08/models/Cliente.java
@@ -0,0 +1,15 @@
+package com.salesianostriana.dam.EC08.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+
+    @GeneratedValue
+    @Id
+    private Long id;
+
+
+}
